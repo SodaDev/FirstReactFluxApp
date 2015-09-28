6b75b77ee604693e883dea49e74e308eb0b082dc
SodaDev <soda@Marcins-MacBook-Pro.local>
2015-09-28 07:14:47 +0200

    Added css and img to build Added lodash, react, react router & flux

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify');         //Bundles JS
 var reactify = require('reactify');             //Transforms React JSX to JS
 var source = require('vinyl-source-stream');    //Use text streams with Gulp
 var concat = require('gulp-concat');            //Concatenates files
+var lint = require('gulp-eslint');              //Lint JS files, including JSX
 var os = require('os');
 
 var config = {
@@ -15,6 +16,7 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        images: './src/img/*',
         css: [
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
             'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -64,12 +66,29 @@ gulp.task('css', function(){
         .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js']);
+gulp.task('images', function(){
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist + '/img'))
+        .pipe(gulpConnect.reload());
+
+    gulp.src('./src/favicon.ico')
+        .pipe(gulp.dest(config.paths.dist));
 });
 
-gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
+gulp.task('lint', function(){
+    return gulp.src(config.paths.js)
+               .pipe(lint({
+                   config: 'eslint.config.json'
+               }))
+               .pipe(lint.format());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'images', 'lint', 'open', 'watch']);
 
 
 //UTILS
